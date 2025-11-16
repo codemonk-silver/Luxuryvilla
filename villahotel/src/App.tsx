1aa8facd58f52d31e8bc9672d6f919593d6a58a3
codemonk-silver <olajugbagbeeniola12@outlook.com>
2025-11-15 18:36:00 -0800

    setting up interface

---
+++ villahotel/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+
+const App = () => {
+  return (
+    <div>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+    </div>
+  )
+}
+
+export default App
